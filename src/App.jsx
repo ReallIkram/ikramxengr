f2c0162b4e8e268fe9af56e7f1ca02d0bd921845
Ikram <ikrambtm444@gmail.com>
2026-01-13 11:06:06 +0500

    testimonials removed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
       <Education />
       {/* <Certifications /> */}
       <Services />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
